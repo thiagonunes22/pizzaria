e743d5631992c8a4f55c5e7604dfb943ec3bd021
Thiago Nunes <thiago.nuneswlm@hotmail.com>
2022-10-19 14:58:24 +0100

    Quarto commit

---
+++ services/UsuariosServices.js
@@ -0,0 +1,71 @@
+const usuarios = require('../databases/usuarios.json')
+const fs = require('fs')
+
+function listar(){
+    console.table(usuarios.map(u => `ID : ${u.id} | Nome: ${u.nome} | Email: ${u.email}`))
+}
+
+
+function salvar(arrayDeUsuarios){
+    fs.writeFileSync('../databases/usuarios.json', JSON.stringify(arrayDeUsuarios,null,4))
+    usuarios.push(arrayDeUsuarios)
+}
+
+function cadastrar(objeto){
+// Seu código aqui
+}
+
+function detalhar(idUsuario){
+// Seu código aqui
+}
+
+function remover(idDoUsuarioParaRemover){
+    // Seu código aqui
+}
+
+function alterar(novosDados, idUsuario){
+    // Seu código aqui
+}
+
+function addEndereco(novoEndereco, idUsuario){
+    // Seu código aqui
+}
+
+function removerEndereco(posicaoDoEndereco, idUsuario){
+// Seu código aqui
+}
+
+function alterarEndereco(posicaoDoEndereco, novoEndereco, idUsuario){
+// Seu código aqui        
+}
+
+function addFormaDePagamento(novaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+function removerFormaDePagamento(posicaoDaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+function alterarFormaDePagamento(novaFormaDePagamento, posicaoDaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+const UsuariosServices = {
+    cadastrar,
+    listar,
+    detalhar,
+    remover,
+    alterar,
+    addEndereco,
+    removerEndereco,
+    alteraEndereco: alterarEndereco,
+    addFormaDePagamento,
+    removerFormaDePagamento,
+    alterarFormaDePagamento
+}
+
+module.exports = UsuariosServices;
+
+listar(usuarios)
+salvar(usuarios)
